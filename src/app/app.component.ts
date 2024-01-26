aa56c77897cfcca4c3e41234b957e4eb311bab27
Sebastian Mera <102315503+centii123@users.noreply.github.com>
2024-01-25 20:29:07 -0500

    examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestion-pagos';
+  products=[{
+    code:"ggg",
+    name:"name",
+    category:"ggg",
+    quantity:"quantity"
+  }]
 }
